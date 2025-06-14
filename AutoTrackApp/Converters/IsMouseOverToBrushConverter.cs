139fc657625a97b24e54ff188e683698401dfdbb
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2025-06-14 21:23:49 +0400

    Нарисован карандаш)

---
+++ AutoTrackApp/Converters/IsMouseOverToBrushConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AutoTrackApp.Converters
+{
+  public class IsMouseOverToBrushConverter : IMultiValueConverter
+  {
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (values.Length == 3 && values[0] is bool isOver && values[1] is Brush normal && values[2] is Brush hover)
+        return isOver ? hover : normal;
+      return DependencyProperty.UnsetValue;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+  }
+}
---
+++ AutoTrackApp/UI/Components/Icons/PencilIcon.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AutoTrackApp.UI.Components.Icons
+{
+  public partial class PencilIcon : UserControl
+  {
+    public PencilIcon()
+    {
+      InitializeComponent();
+    }
+
+    public static readonly DependencyProperty IconFillProperty =
+        DependencyProperty.Register(nameof(IconFill), typeof(Brush), typeof(PencilIcon),
+            new PropertyMetadata(Brushes.Blue));
+
+    public Brush IconFill
+    {
+      get => (Brush)GetValue(IconFillProperty);
+      set => SetValue(IconFillProperty, value);
+    }
+
+    public static readonly DependencyProperty HoverFillProperty =
+        DependencyProperty.Register(nameof(HoverFill), typeof(Brush), typeof(PencilIcon),
+            new PropertyMetadata(new SolidColorBrush(Color.FromRgb(37, 99, 235)))); // #2563eb
+
+    public Brush HoverFill
+    {
+      get => (Brush)GetValue(HoverFillProperty);
+      set => SetValue(HoverFillProperty, value);
+    }
+
+    public static readonly DependencyProperty SizeProperty =
+        DependencyProperty.Register(nameof(Size), typeof(double), typeof(PencilIcon),
+            new PropertyMetadata(24.0, OnSizeChanged));
+
+    public double Size
+    {
+      get => (double)GetValue(SizeProperty);
+      set => SetValue(SizeProperty, value);
+    }
+
+    private static void OnSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+      if (d is PencilIcon icon)
+      {
+        icon.Width = icon.Size;
+        icon.Height = icon.Size;
+      }
+    }
+  }
+}
